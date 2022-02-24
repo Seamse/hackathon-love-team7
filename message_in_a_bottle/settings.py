1e19df45b27dda0336b9f2061ee8e020672e2375
Daniel Callaghan <dan_cal89@hotmail.com>
2022-02-24 23:09:37 +0000

    add user registration, login and logout functionality

--- message_in_a_bottle/settings.py
+++ message_in_a_bottle/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'cloudinary',
     'home',
     'bottles',
@@ -145,3 +146,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'landing-home'
